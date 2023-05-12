6db744a10b933bf66c9092a82c3ebe371cf84f9d
Nadin Tamer <17547686+nadintamer@users.noreply.github.com>
2023-05-12 13:50:52 -0700

    add tests for subtraction and division

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,8 +3,11 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
+# Checks that the program outputs "-1" for an input of "2 - 3"
+assert run("2 - 3").output == -1
 
-
+# Checks that the program outputs "4" for an input of "8 / 2"
+assert run("8 / 2").output == 4
 ###
 
 print("All tests passed!")
